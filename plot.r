b1b149382f8c6136319b0b80a351afe9dd233ed7
Stefan Heyder <stefan.heyder@gmail.com>
2020-04-03 13:49:48 +0200

    Improve legend of plots

--- plot.r
+++ plot.r
@@ -6,24 +6,10 @@ plot_repronum <- function(estimates, country_name) {
     plot_ly(estimates, x= ~date, y= ~repronum) %>%
         add_lines(name = "reproduction number") %>%
         add_ribbons(ymin = ~ci.lower, ymax = ~ci.upper, opacity = .5, name = "95% confidence interval") %>%
-        add_bars(y = ~new.cases, yaxis = "y2", opacity = .1, name = "new cases") %>%
+        add_lines(y = ~1, name = "R = 1", opacity = .3, line = list(dash = "dash")) %>%
+        add_bars(y = ~new.cases, yaxis = "y2", opacity = .1, name = "newly reported cases") %>%
         layout(
-            shapes = list(
-                list(
-                    type = "line",
-                    x0 = 0, x1 = 1,
-                    xref = "paper",
-                    y0 = 1, y1 = 1,
-                    line = list(color = "red", dash = "dot")
-                )#,
-                #                list(
-                #                    type = "line",
-                #                    x0 = 0, x1 = 1,
-                #                    xref = "paper",
-                #                    y0 = 2.4, y1 = 2.4,
-                #                    line = list(color = "red", dash = "dot")
-                #                )
-                ),
+            title = "Estimated reproduction number / reported cases",
             yaxis = list(
                 type = "log",
                 title = "reproduction rate",
@@ -31,7 +17,7 @@ plot_repronum <- function(estimates, country_name) {
                 tickvals = 1:10,
                 range = log(c(min(c(0.3, estimates$ci.lower), na.rm = TRUE), 10), base = 10)
                 ),
-            colorway = c("black", "grey", "blue"),
+            colorway = c("black", "grey", "red", "blue"),
             yaxis2 = list(
                 overlaying = "y",
                 side = "right",
@@ -46,10 +32,11 @@ plot_repronum <- function(estimates, country_name) {
                 tickformat = "%d/%m"
                 ),
             legend = list(
-                x = 0,
-                y = -0.3,
+                x = 0.2,
+                y = -0.15,
                 font = list(size = 10),
-                bgcolor = "#FFFFFF00"
+                bgcolor = "#FFFFFF00",
+                orientation = "h"
                 ),
             margin = list(r = 60)
         )
