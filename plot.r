0425e2ac5609b75b7876e6d6ac5991346cde62a4
Stefan Heyder <stefan.heyder@gmail.com>
2020-04-03 10:00:37 +0200

    Improve plots

--- plot.r
+++ plot.r
@@ -4,9 +4,9 @@ plot_repronum <- function(estimates, country_name) {
 
     first_monday <- ymd("2020-01-06")
     plot_ly(estimates, x= ~date, y= ~repronum) %>%
-        add_bars(y = ~new.cases, yaxis = "y2", opacity = .3, name = "new cases") %>%
-        add_lines(name = "R(t)") %>%
-        add_ribbons(ymin = ~ci.lower, ymax = ~ci.upper, opacity = .5, name = "confidence sets") %>%
+        add_lines(name = "reproduction number") %>%
+        add_ribbons(ymin = ~ci.lower, ymax = ~ci.upper, opacity = .5, name = "95% confidence interval") %>%
+        add_bars(y = ~new.cases, yaxis = "y2", opacity = .1, name = "new cases") %>%
         layout(
             shapes = list(
                 list(
@@ -15,23 +15,23 @@ plot_repronum <- function(estimates, country_name) {
                     xref = "paper",
                     y0 = 1, y1 = 1,
                     line = list(color = "red", dash = "dot")
-                    )#,
-#                list(
-#                    type = "line",
-#                    x0 = 0, x1 = 1,
-#                    xref = "paper",
-#                    y0 = 2.4, y1 = 2.4,
-#                    line = list(color = "red", dash = "dot")
-#                )
+                )#,
+                #                list(
+                #                    type = "line",
+                #                    x0 = 0, x1 = 1,
+                #                    xref = "paper",
+                #                    y0 = 2.4, y1 = 2.4,
+                #                    line = list(color = "red", dash = "dot")
+                #                )
                 ),
             yaxis = list(
                 type = "log",
-                title = "R(t)",
+                title = "reproduction rate",
                 tickmode = "array",
                 tickvals = 1:10,
                 range = log(c(min(c(0.3, estimates$ci.lower), na.rm = TRUE), 10), base = 10)
                 ),
-            colorway = c("blue", "black", "grey"),
+            colorway = c("black", "grey", "blue"),
             yaxis2 = list(
                 overlaying = "y",
                 side = "right",
@@ -39,11 +39,18 @@ plot_repronum <- function(estimates, country_name) {
                 ),
             xaxis =  list(
                 ticks = "outside",
-                tickvals = seq(first_monday, today(), by = "1 day"),
+                tickvals = seq(first_monday, today(), by = "1 week"),
                 showline = TRUE,
                 showgrid = TRUE,
                 type = "date",
-                tickformat = "%d-%m"
-            )
+                tickformat = "%d/%m"
+                ),
+            legend = list(
+                x = 0,
+                y = -0.3,
+                font = list(size = 10),
+                bgcolor = "#FFFFFF00"
+                ),
+            margin = list(r = 60)
         )
 }
