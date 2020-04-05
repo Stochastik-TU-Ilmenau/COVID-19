3d41e6964405c3c3f81f3b4348a94aefc4b5fc1a
Matthias Glock <matthias.glock@tu-ilmenau.de>
2020-04-05 21:11:25 +0200

    switched too arcgis query api for RKI data

---
+++ data/rki_query.py
@@ -0,0 +1,92 @@
+
+import os
+import json
+import csv
+import io
+import sys
+import pytz
+import urllib.parse
+from datetime import datetime, timedelta
+from itertools import zip_longest
+
+import pandas as pd
+import requests
+
+STATE_NAME_ISONAME_MAP = {
+    "Baden-Württemberg": "DE-BW",
+    "Bayern": "DE-BY",
+    "Brandenburg": "DE-BB",
+    "Berlin": "DE-BE",
+    "Bremen": "DE-HB",
+    "Hamburg": "DE-HH",
+    "Hessen": "DE-HE",
+    "Mecklenburg-Vorpommern": "DE-MV",
+    "Niedersachsen": "DE-NI",
+    "Nordrhein-Westfalen": "DE-NW",
+    "Rheinland-Pfalz": "DE-RP",
+    "Saarland": "DE-SL",
+    "Sachsen-Anhalt": "DE-ST",
+    "Sachsen": "DE-SN",
+    "Schleswig-Holstein": "DE-SH",
+    "Thüringen": "DE-TH",
+}
+
+
+def arcgis_query(offset = 0, chunk_size = 1):
+    AG_RKI_SUMS_QUERY_BASE_URL = "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/query?"
+
+    params = urllib.parse.urlencode(
+    {
+            "where": "1=1",
+            "returnGeometry": "false",
+            "outFields": "*",
+            "orderByFields": "Meldedatum asc",
+            "resultOffset": offset,
+            "resultRecordCount": chunk_size,
+            "f": "json",
+        }
+    )
+
+    url = f"{AG_RKI_SUMS_QUERY_BASE_URL}{params}"
+    #print(url)
+
+    resp = requests.get(url)
+    resp.raise_for_status()
+    data = resp.json()
+    return data
+
+data = arcgis_query()
+
+#with open('chunk_' + str(offset) + '.json', 'w') as f:
+#    json.dump(data, f)
+
+header = data["fields"]
+#cases = data["features"]
+
+header = [h["name"] for h in header]
+#print(header)
+
+#data = json.loads(data)
+f = csv.writer(open("rki_data/RKI_COVID19.csv", "w+"))
+# Write CSV Header, If you dont need that, remove this line
+f.writerow(header)
+
+# how too convert the strange arcgis time format:
+# datetime.fromtimestamp(int(query["Meldedatum"] / 1000.0))
+
+def csv_add_lines(file, cases):
+    for c in cases:
+        row = [c["attributes"][h] for h in header]
+        row[8] = datetime.fromtimestamp(int(row[8] / 1000.0))
+        # print(row)
+        file.writerow(row)
+
+chunk_size = 2000
+offset = 0
+while(True):
+    data = arcgis_query(offset=offset, chunk_size=chunk_size)
+    cases = data["features"]
+    if not cases:
+        break
+    csv_add_lines(f, cases)
+    offset += chunk_size
